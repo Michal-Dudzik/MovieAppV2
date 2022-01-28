58b60fab1a6469c39ded037f30cdf328c10b814a
Michal-Dudzik <dumichal20@wp.pl>
2022-01-28 16:29:39 +0100

    MovieData controller adjusted

--- MovieApp/Controllers/MovieDatasController.cs
+++ MovieApp/Controllers/MovieDatasController.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿#nullable disable
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,19 +17,21 @@ namespace MovieApp.Controllers
     public class MovieDatasController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public MovieDatasController(ApplicationDbContext context)
+        public MovieDatasController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            this._hostEnvironment = hostEnvironment;
         }
 
-        // GET: MovieDatas
+        // GET: MovieData
         public async Task<IActionResult> Index()
         {
             return View(await _context.MovieData.ToListAsync());
         }
 
-        // GET: MovieDatas/Details/5
+        // GET: MovieData/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -43,21 +49,33 @@ namespace MovieApp.Controllers
             return View(movieData);
         }
 
-        // GET: MovieDatas/Create
+        // GET: MovieData/Create
+        [Authorize(Roles = "Administrator")]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: MovieDatas/Create
+        // POST: MovieData/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MovieId,MovieName,MovieDirector,ReleaseDate,Genre,Description,ImageName")] MovieData movieData)
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Create([Bind("MovieId,MovieName,MovieDirector,ReleaseDate,Genre,Description,ImageFile")] MovieData movieData)
         {
-            if (ModelState.IsValid)
             {
+                //Save image to wwwroot/image
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(movieData.ImageFile.FileName);
+                string extension = Path.GetExtension(movieData.ImageFile.FileName);
+                movieData.ImageName = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await movieData.ImageFile.CopyToAsync(fileStream);
+                }
+                //Insert record
                 _context.Add(movieData);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -65,7 +83,8 @@ namespace MovieApp.Controllers
             return View(movieData);
         }
 
-        // GET: MovieDatas/Edit/5
+        // GET: MovieData/Edit/5
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,11 +100,12 @@ namespace MovieApp.Controllers
             return View(movieData);
         }
 
-        // POST: MovieDatas/Edit/5
+        // POST: MovieData/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Edit(int id, [Bind("MovieId,MovieName,MovieDirector,ReleaseDate,Genre,Description,ImageName")] MovieData movieData)
         {
             if (id != movieData.MovieId)
@@ -116,7 +136,8 @@ namespace MovieApp.Controllers
             return View(movieData);
         }
 
-        // GET: MovieDatas/Delete/5
+        // GET: MovieData/Delete/5
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,12 +155,19 @@ namespace MovieApp.Controllers
             return View(movieData);
         }
 
-        // POST: MovieDatas/Delete/5
+        // POST: MovieData/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var movieData = await _context.MovieData.FindAsync(id);
+
+            //delete image from wwwroot/image
+            var imagePath = Path.Combine(_hostEnvironment.WebRootPath, "image", movieData.ImageName);
+            if (System.IO.File.Exists(imagePath))
+                System.IO.File.Delete(imagePath);
+
             _context.MovieData.Remove(movieData);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
