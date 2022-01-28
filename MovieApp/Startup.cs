d28d3cb65c10bcb6f91e09d7b993440041d6e5d4
Michal-Dudzik <dumichal20@wp.pl>
2022-01-28 16:15:41 +0100

    shared layout

--- MovieApp/Startup.cs
+++ MovieApp/Startup.cs
@@ -68,7 +68,7 @@ namespace MovieApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=MovieDatas}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
